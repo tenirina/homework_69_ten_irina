29b35dead5361b972df2e189c79f109e5cf20aaf
tenirina <virhot17@gmail.com>
2022-11-17 22:58:19 +0600

    Add
    Добавлены 4 точки доступа для отработки запроса сложение, вычитания, умножение и деление. Отработаны возможные ошибки.

---
+++ app/webapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from webapp.views import add_view, subtract_view, multiply_view, divide_view
+
+urlpatterns = [
+    path('add/', add_view, name='add'),
+    path('subtract/', subtract_view, name='subtract'),
+    path('multiply/', multiply_view, name='multiply'),
+    path('divide/', divide_view, name='divide')
+]
--- app/webapp/views.py
+++ app/webapp/views.py
@@ -1,3 +1,84 @@
-from django.shortcuts import render
 
-# Create your views here.
+import json
+from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+def add_view(request, *args, **kwargs):
+    if request.method == 'POST' and request.body:
+        try:
+            num = json.loads(request.body)
+            number_a = int(num['A'])
+            number_b = int(num['B'])
+            add = number_a + number_b
+            response = JsonResponse({'add': add})
+            print(response)
+            response.status_code = 200
+        except ValueError:
+            response_data = {'error': 'Incorrect number format!'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        return response
+
+
+def subtract_view(request, *args, **kwargs):
+    if request.method == 'POST' and request.body:
+        try:
+            num = json.loads(request.body)
+            number_a = int(num['A'])
+            number_b = int(num['B'])
+            subtract = number_a - number_b
+            response = JsonResponse({'subtract': subtract})
+            print(response)
+            response.status_code = 200
+        except ValueError:
+            response_data = {'error': 'Incorrect number format!'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        return response
+
+
+def multiply_view(request, *args, **kwargs):
+    if request.method == 'POST' and request.body:
+        try:
+            num = json.loads(request.body)
+            number_a = int(num['A'])
+            number_b = int(num['B'])
+            multiply = number_a * number_b
+            response = JsonResponse({'multiply': multiply})
+            print(response)
+            response.status_code = 200
+        except ValueError:
+            response_data = {'error': 'Incorrect number format!'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        return response
+
+
+def divide_view(request, *args, **kwargs):
+    if request.method == 'POST' and request.body:
+        try:
+            num = json.loads(request.body)
+            number_a = int(num['A'])
+            number_b = int(num['B'])
+            divide = number_a / number_b
+            response = JsonResponse({'divide': divide})
+            print(response)
+            response.status_code = 200
+        except ValueError:
+            response_data = {'error': 'Incorrect number format!'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        except ZeroDivisionError:
+            response_data = {'error': 'Division by zero!'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+        return response
+
+
+@ensure_csrf_cookie
+def get_token(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed(f'Not allowed method {request.method}')
+
