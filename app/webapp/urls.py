f6c73fa6b9a262089c3e8c0e5b20a5470a0735cf
tenirina <virhot17@gmail.com>
2022-11-18 15:01:52 +0600

    Add
    добавлен файл index.html для задания2. Внесены начальные данные для отображения

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import add_view, subtract_view, multiply_view, divide_view
+from webapp.views import add_view, subtract_view, multiply_view, divide_view, IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('add/', add_view, name='add'),
     path('subtract/', subtract_view, name='subtract'),
     path('multiply/', multiply_view, name='multiply'),
